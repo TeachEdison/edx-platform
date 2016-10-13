83e73b169a1f9571fb393d9ad92a791822cf0ba6
Sivakumar <sivakumar@teachedison.com>
2016-10-13 13:30:15 +0530

    Revert "Chaging Pltform Name"
    This reverts commit 692e5369fad416ef09a8e433308943458c4b015c.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -46,13 +46,13 @@ from lms.djangoapps.lms_xblock.mixin import LmsBlockMixin
 
 ################################### FEATURES ###################################
 # The display name of the platform to be used in templates/emails/etc.
-PLATFORM_NAME = "TeachEdison"
+PLATFORM_NAME = "Your Platform Name Here"
 CC_MERCHANT_NAME = PLATFORM_NAME
 # Shows up in the platform footer, eg "(c) COPYRIGHT_YEAR"
 COPYRIGHT_YEAR = "2016"
 
-PLATFORM_FACEBOOK_ACCOUNT = "http://www.facebook.com/TeachEdison"
-PLATFORM_TWITTER_ACCOUNT = "@TeachEdison"
+PLATFORM_FACEBOOK_ACCOUNT = "http://www.facebook.com/YourPlatformFacebookAccount"
+PLATFORM_TWITTER_ACCOUNT = "@YourPlatformTwitterAccount"
 
 ENABLE_JASMINE = False
 
